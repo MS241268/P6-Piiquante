cc8338bbcd9bedf3be21d1b8d8fc31499632b826
Schonne Marc <schonne.marc@orange.fr>
2022-11-05 16:48:54 +0100

    Initialisation du projet : Installation des pakages 'Mongoose','Express' & 'Dotenv' +  Création du serveur Node + Création & Configuration de la data base MongoDB + Création fichier '.env' pour les données d'envorionnement

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express')//Importation du package 'express'
+const app = express ()//Création application
+const dotenv = require('dotenv')//Importation du package 'dotenv'
+dotenv.config()
+const MyUrlOfDataBase = process.env.MyUrlOfDataBase//Chargement de la variable d'environnement 'MyUrlOfDataBase' situé de le fichier '.env'
+const mongoose = require('mongoose')//Importation du package 'mongoose'
+
+//Connection API à la base de données MongoDB
+mongoose.connect(MyUrlOfDataBase,
+    //{ useNewUrlParser: true,//Plus nécessaire depuis mongoose 6 par défaut à 'true'
+    //useUnifiedTopology: true }//Plus nécessaire depuis mongoose 6 par défaut à 'true'
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
+/****/
+
+app.use((req, resp, next) => {
+    console.log('Requête reçue !')
+    next()//Renvoi vers le prochain middleware (fonction)
+})
+
+app.use ((req, resp, next) => {
+    resp.status(201) //Modification du code de réponse 200 => 201
+    next()//Renvoi vers le prochain middleware (fonction)
+})
+
+app.use((req, resp, next) => {
+    resp.json({ message: 'Votre requête a bien été reçue !!!'})
+    next()//Renvoi vers le prochain middleware (fonction)
+})
+
+app.use((req, resp) => {//Pas de méthode "next" car c'est le dernier middleware
+    console.log('Réponse envoyée avec succès !')
+})
+
+module.exports = app //Accès de cet application aux autres fichiers notament le serveur Node
+
---
+++ backend/server.js
@@ -0,0 +1,57 @@
+const http = require('http')//Importation du package hhtp de node
+const app = require('./app')
+
+//Renvoi d'un port valide
+const normalizePort = val => {
+    const port = parseInt(val, 10)//Conversion du port 'string' => nbre entier en base 10
+    
+    //Vérification si le port est un nombre et >= 0
+    if (isNaN(port)) {
+      return val
+    }
+    if (port >= 0) {
+      return port
+    }
+    return false
+    /****/
+  }
+/****/
+
+//Détermination du port sur lequel "app" va s'éxécuter
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
+/****/
+
+//Recherche des erreurs serveur
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error
+    }
+    const address = server.address()
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.')
+        process.exit(1)
+        break
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.')
+        process.exit(1)
+        break
+      default:
+        throw error
+    }
+  }
+/****/
+
+//création du serveur
+const server = http.createServer(app)
+
+server.on('error', errorHandler)
+server.on('listening', () => {
+  const address = server.address()
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port
+  console.log('Listening on ' + bind)
+})
+
+server.listen(port)//Ecoute du serveur sur un port valide si le 3000 est déjà utilisé sinon 3000 par défaut
