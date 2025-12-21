e6f8c241eac901bf2d839e415a4cf202435a0397
Schonne Marc <schonne.marc@orange.fr>
2025-12-21 16:40:41 +0100

    Correction de 'multer-config.js' : choix du dossier 'images' ou 'images_prod' en focntion de variable environnement 'NODE_ENV' pour  Render + Correction de 'app.js' : suppression de 'app.use(/'images'...) qui créait l'écrasement du middleware qui choisit entre 'images'(version locale) ou 'images_prod'(version render)

--- backend/app.js
+++ backend/app.js
@@ -87,7 +87,7 @@ app.get('/ping', (req, res) => {
 });
 
 //Importation de la route 'images'
-app.use('/images', express.static(path.join(__dirname, 'images')));
+// app.use('/images', express.static(path.join(__dirname, 'images')));
 /****/
 
 //Enregistrement du routeur 'user'
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -9,18 +9,14 @@ const MIME_TYPES = {
 //Création objet de configuration pour multer
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    // callback(null, 'images'); //Callback sur le dossier 'images'
-    if (process.env.NODE_ENV === 'production') {
-      callback(null, 'images_prod'); // dossier prod
-    } else {
-      callback(null, 'images'); // dossier local
-    }
+    const folder =
+      process.env.NODE_ENV === 'production' ? 'images_prod' : 'images';
+    callback(null, path.join(__dirname, folder));
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_'); //Renommage du fichier en remplaçant ' ' par '_'
     const extension = MIME_TYPES[file.mimetype]; //Extension du fichier
-    // callback(null, name + Date.now() + '.' + extension)//Fichier avec nom + horodatage (permet que l'image soit unique) + '.' + extension
-    callback(null, file.originalname);
+    callback(null, name + Date.now() + '.' + extension); //Fichier avec nom + horodatage (permet que l'image soit unique) + '.' + extension
   },
 });
 /****/
